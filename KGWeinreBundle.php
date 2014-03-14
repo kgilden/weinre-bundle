6a0de9376fd46ef2ce448b93a90ab1478bdea311
Kristen Gilden <kristen.gilden@gmail.com>
2014-03-14 21:47:14 +0200

    Create Bundle class

---
+++ KGWeinreBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the KGWeinreBundle package.
+ *
+ * (c) Kristen Gilden <kristen.gilden@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KG\WeinreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Kristen Gilden <kristen.gilden@gmail.com>
+ */
+class KGWeinreBundle extends Bundle
+{
+}
